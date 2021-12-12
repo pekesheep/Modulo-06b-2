775acbf64389b5de0d95ad6879de6b16a6aeb252
unknown <pekesheep@gmail.com>
2021-12-12 12:48:13 +0100

    Laboratorio 06b - 2

--- script.js
+++ script.js
@@ -1,31 +1,23 @@
-const plainAlphabet = "abcdefghijklmnopqrstuvwxyz:()!¡,'";
-const encryptedAlphabet = "qw,ert(yuio'pa:sdfg!hjklz¡xcv)bnm";
+var randomPick = (n, min, max) => {
 
-//Botones deshabilitados si no hay texto
-activateBtn = () => {
-  document.getElementById("encrypt-btn").disabled = document.getElementById("encrypt-text").value === "" ? true : false;
-  document.getElementById("decrypt-btn").disabled = document.getElementById("decrypt-text").value === "" ? true : false;
+    
+    const range = max - min + 1;
+    var random = [];
+
+    // Si pedimos más números de los posibles no ejecutamos.
+    if (n > range) return console.log ("La cantidad de números solicitados es mayor que el rango dado.");
+
+    // Generador de números aleatorios según rango dado.
+    for (var i = 0; i < n; i++) {
+        var control = Math.floor(Math.random() * range) + min;
+        
+        // Control de números repetidos
+        random.indexOf(control, 0) === -1 ? random[i] = control : i--;       
+    }
+
+    console.log("Final ", random);
 }
-//Encriptar texto
-var encryptText = () => {
-  var result = "";
-  var text = document.getElementById("encrypt-text").value;
-  for (var i = 0; i < text.length; i++) {
-    var indexAlphabet = plainAlphabet.indexOf(text[i]);
-    if (indexAlphabet !== -1) result = result + encryptedAlphabet[indexAlphabet];
-    else result = result + text[i];
-  }
-  document.getElementById("decrypt-text").value = result;
-}
-//Desencriptar texto
-var decryptText = () => {
-  var result = "";
-  var text = document.getElementById("decrypt-text").value;
-  for (var i = 0; i < text.length; i++) {
-    var indexAlphabet = encryptedAlphabet.indexOf(text[i]);
-    if (indexAlphabet !== -1) result = result + plainAlphabet[indexAlphabet];
-    else result = result + text[i];
-  }
-  document.getElementById("encrypt-text").value = result;
-}
+
+randomPick(10, 1, 5);
+
 
